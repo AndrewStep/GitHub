2488548428805eecb3ed60ac6f795c15799a8131
ffeelex <ffeelex@gmail.com>
2017-12-18 01:40:21 +0200

    123

---
+++ CarStore/CarStore/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarStore.WEBUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Summary()//передать сюда модель откуда будут братся пароли
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ CarStore/CarStore/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarStore.DAL.Entities;
+
+namespace CarStore.WEBUI.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+    }
+}
