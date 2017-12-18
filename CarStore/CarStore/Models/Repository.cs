573d92ed919c0e0072d4b4e93191872694b43bfb
student <student@K42-07>
2017-12-18 21:23:06 +0200

    DB

---
+++ CarStore/CarStore/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarStore.Models
+{
+    public class Repository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Car> Cars
+        {
+            get { return context.Cars; }
+        }
+    }
+}
